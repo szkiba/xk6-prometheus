a7528674d8582c3999b771842d654abd0d2e36aa
Ivan SZKIBA <szkiba@users.noreply.github.com>
2023-05-22 17:03:41 +0200

    refactor: fix linter issues

--- prometheus.go
+++ prometheus.go
@@ -1,24 +1,6 @@
-// MIT License
+// SPDX-FileCopyrightText: 2021 - 2023 Iván Szkiba
 //
-// Copyright (c) 2021 Iván Szkiba
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// SPDX-License-Identifier: MIT
 
 package prometheus
 
@@ -27,12 +9,12 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"time"
 
+	"github.com/gorilla/schema"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
 	"github.com/szkiba/xk6-prometheus/internal"
-
-	"github.com/gorilla/schema"
 	"go.k6.io/k6/output"
 )
 
@@ -58,26 +40,26 @@ type Output struct {
 	logger logrus.FieldLogger
 }
 
-func New(params output.Params) (output.Output, error) {
+func New(params output.Params) (output.Output, error) { // nolint:ireturn
 	registry, ok := prometheus.DefaultRegisterer.(*prometheus.Registry)
 	if !ok {
 		registry = prometheus.NewRegistry()
 	}
 
-	o := &Output{
+	out := &Output{ // nolint:exhaustruct
 		PrometheusAdapter: internal.NewPrometheusAdapter(registry, params.Logger, "", ""),
 		arg:               params.ConfigArgument,
 		logger:            params.Logger,
 	}
 
-	return o, nil
+	return out, nil
 }
 
 func (o *Output) Description() string {
 	return fmt.Sprintf("prometheus (%s)", o.addr)
 }
 
-func getopts(qs string) (*options, error) {
+func getopts(query string) (*options, error) {
 	opts := &options{
 		Port:      defaultPort,
 		Host:      "",
@@ -85,18 +67,18 @@ func getopts(qs string) (*options, error) {
 		Subsystem: "",
 	}
 
-	if qs == "" {
+	if query == "" {
 		return opts, nil
 	}
 
-	v, err := url.ParseQuery(qs)
+	values, err := url.ParseQuery(query)
 	if err != nil {
 		return nil, err
 	}
 
 	decoder := schema.NewDecoder()
 
-	if err = decoder.Decode(opts, v); err != nil {
+	if err = decoder.Decode(opts, values); err != nil {
 		return nil, err
 	}
 
@@ -119,7 +101,9 @@ func (o *Output) Start() error {
 	}
 
 	go func() {
-		if err := http.Serve(listener, o.PrometheusAdapter.Handler()); err != nil {
+		server := &http.Server{Handler: o.PrometheusAdapter.Handler(), ReadHeaderTimeout: time.Second} //nolint:exhaustruct
+
+		if err := server.Serve(listener); err != nil {
 			o.logger.Error(err)
 		}
 	}()
